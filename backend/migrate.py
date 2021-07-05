42c3a0114aded03595ad3cbe915bd0bd4b45159b
Eduardo Mascari <mascari.eduardo@gmail.com>
2021-07-05 23:32:12 +0100

    Removing remote MongoDB instance

--- backend/migrate.py
+++ backend/migrate.py
@@ -2,7 +2,7 @@
 import pymongo
 
 
-client = pymongo.MongoClient("mongodb+srv://testview:sdf56JKL!@test-stsy1.gcp.mongodb.net/test?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb://localhost:27017/test?retryWrites=true&w=majority")
 
 mydb = client['all_projects']
 mycol = mydb['projects']
