ae841ed75c82d2ce6b8d10e5e231c8abc23f059a
Caique Noboa <caique.noboa@mercadolivre.com>
2021-09-18 20:59:30 -0300

    generate the complexity average

--- backend/env/lib/python3.9/site-packages/pydriller/__init__.py
+++
@@ -1,5 +0,0 @@
-# pylint: disable=C0111
-from .domain.commit import Commit, Modification, ModificationType # noqa
-from .repository_mining import RepositoryMining, GitRepository    # noqa
-
-__version__ = "1.15.5"
